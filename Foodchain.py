ccb7586a50eb59700459fef7ce5cfef1aaf7faac
Robel kebede <robelkebede44@gmail.com>
2019-04-22 16:45:38 +0300

    Create and Transfer Asset

--- Foodchain.py
+++ Foodchain.py
@@ -1,13 +1,17 @@
 import requests
 import mysql.connector
+from bigchaindb_driver import BigchainDB
 import json
 #import bip39
+
+bdb_root_url = "http://127.0.0.1:9984"
 
 class foodchain:
     def __init__(self,url,username,pass_key):
         self.url = url
         self.username = username
         self.pass_key = pass_key
+        self.bdb = BigchainDB(bdb_root_url)
 
 
         mydb = mysql.connector.connect(
@@ -37,15 +41,47 @@ class foodchain:
 
     def create(self,grower,company,package):
         #create a transaction
-        create_transaction = requests.post(self.url+":8000/new_asset_api",data={'grower':grower,
-                                                                       'package':package,
-                                                                       'company':company,
-                                                                       }).text
         #use bigchaindb_driver api 
-        #how to send the key alongside with the data
+
+        FoodItem={'grower':grower,
+                        'package':package,
+                        'company':company,
+                        'id':'4242'
+                 }
+        metaData ={
+                "action":"new asset api",
+                "date":"timestamp"
+                }
+
+        assetData = {
+                "data":{
+                "type":"Food",
+                "item":FoodItem
+            }
+        }
+
+        prepared_creation_tx = self.bdb.transactions.prepare(
+
+            operation="CREATE",
+            signers = self.public_key,
+            asset = assetData,
+            metadata = metaData
+
+        )
 
 
-        print(create_transaction)
+        fulfilled_creation_tx = self.bdb.transactions.fulfill(
+                prepared_creation_tx, private_keys=self.private_key)
+
+
+        sent_creation_tx = self.bdb.transactions.send_commit(fulfilled_creation_tx)
+        print(sent_creation_tx == fulfilled_creation_tx)
+
+        print(fulfilled_creation_tx)
+
+        #I THINK THE PROBLEM WITH TRANSFER ASSET AND CREATE TRANSACTION IN API ARE THE SAME
+
+        return fulfilled_creation_tx
 
         #if the transaction is succesful return txid
         #get the data from the response
@@ -56,19 +92,62 @@ class foodchain:
         #return all asset from javascript json response
         pass
 
-    def transfer(self,tx_id):
+    def transfer(self,tx_id,send_to):
         #accepts private key and pulic key
-        transfer_asset = requests.post(self.url+"/transfer",data={'tx_id':tx_id,
-                                                                  'public_key':self.key.public_key
-                                                                 })
+        """transfer_asset = requests.post(self.url+":8000/transfer",data={'tx_id':tx_id,
+                                                    'public_key':self.public_key,
+                                                                   'private':self.private_key
+                                                                 }) """
+
+        creation_tx = self.bdb.transactions.retrieve(tx_id)
+
+        asset_id  =creation_tx['id']
+
+
+        transfer_asset = {
+                'id':asset_id
+            }
+
+        output_index = 0
+
+        output = creation_tx['outputs'][output_index]
+
+        transfer_input = {
+               'fulfillment': output['condition']['details'],
+                'fulfills': {
+                'output_index': output_index,
+                'transaction_id': creation_tx['id'],
+                },
+             'owners_before': output['public_keys'],
+        }
+
+        prepared_transfer_tx = self.bdb.transactions.prepare(
+            operation='TRANSFER',
+            asset=transfer_asset,
+            inputs=transfer_input,
+            recipients=send_to,
+         )
+
+        fulfilled_transfer_tx = self.bdb.transactions.fulfill(
+              prepared_transfer_tx,
+              private_keys=self.private_key,
+         )
+
+        sent_transfer_tx = self.bdb.transactions.send_commit(fulfilled_transfer_tx)
+         
+        print(sent_transfer_tx == fulfilled_transfer_tx)
+
         #returns sucesses
         #DONE
         pass
     def view_asset(self,tx_id):
         #detail for a single item thes thing
-        pass
+        name=self.bdb.assets.get(search=key)
+        print(name)
 
-    def history(self,tx_id):
+    def history(self,key):
+        #this is the most confusing 
+
         #returns the previous holders of the assets
         print("this is a tes for the history")
 
--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 
 from Foodchain import foodchain
+import datetime
+import time
+
 
 fc = foodchain("http://127.0.0.1",'corp','ftf')
 
-#transfer assets in the car
 
+#print(datetime.datetime.now())
+#print(dir(datetime.datetime))
+#print(datetime.datetime.timestamp())
+
+#fc.create("test","test","test")
+
+
+fc.transfer("0cf25382262d26ea02c9798923e87e60e23870eafcfa6bba1ed1658e9b2fe206","DgtLE5EveLUpaKJ55KLWpvHutvuLYGmZfcC1Gw9stLHq")
+
+#fc.view_asset("Food")
 #print(dir(foodchain))
 
-print(fc.history("334565432"))
+#print(fc.history("334565432"))
 
